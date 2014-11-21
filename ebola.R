626a3c7cd6d0445b11219cfc5b5bf2d6f75a4ddf
kaz-yos <kazky@mac.com>
2014-11-21 17:20:52 -0500

    Add template file

---
+++ ebola.R
@@ -0,0 +1,45 @@
+#!/usr/bin/Rscript
+
+################################################################################
+### Hack Ebola Hackathon
+## 
+## Created on: 2014-11-21
+## Author: Kazuki Yoshida
+################################################################################
+
+
+### Prepare environment
+################################################################################
+
+## Configure parallelization
+library(doMC)           # Parallel backend to foreach (used in plyr)
+registerDoMC()          # Turn on multicore processing
+options(cores = 4)
+options(mc.cores = 4)
+
+## Load packages
+library(magrittr)
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+library(survival)
+
+## Configure sink()
+if (sink.number() != 0) {sink()}
+..scriptFileName.. <- gsub("^--file=", "", Filter(function(x) {grepl("^--file=", x)}, commandArgs()))
+if (length(..scriptFileName..) == 1) {
+    sink(file = paste0(..scriptFileName.., ".txt"), split = TRUE)
+}
+options(width = 120)
+
+
+### Load data
+################################################################################
+
+
+
+################################################################################
+## Record package versions
+print(sessionInfo())
+## Stop sinking to a file if active
+if (sink.number() != 0) {sink()}
