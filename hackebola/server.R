0a666c4fd750d29eda670ec0a410a8617750dfba
kaz-yos <kazky@mac.com>
2014-11-22 11:02:50 -0500

    Add title based on the number of days since 1900

--- hackebola/server.R
+++ hackebola/server.R
@@ -3,6 +3,13 @@ library(ggplot2)
 library(ggmap)
 
 
+### Load time series dataset
+## Load the data file available at:
+## http://www.qdatum.io/public-sources
+ebolaTimeSeries <- read.delim("./data/2.csv", header = TRUE)
+## Fix
+## ebolaTimeSeries$date <- ebolaTimeSeries$date + as.Date("1899-12-31")
+
 ### Load geocode dataset
 ## Used to create a smaller dataset from 1.csv
 if (FALSE) {
@@ -24,18 +31,24 @@ shinyServer(function(input, output) {
     ## Create a reactive dataset 
     ## reactive() is just creating a thunk (delayed execution)
     datasetThunk <- reactive(function() {
+        ## Subset dataset based on the max date
+        ## maxdate <- input$maxdate + as.Date("1899-12-31")
         ebolaTimeSeries[ebolaTimeSeries$date <= input$maxdate,]
     })
 
     ## Plot thunk creation
     output$plot <- renderPlot(function() {
-
+        ## Max date creation
+        ## maxdateStr <- input$maxdate + as.Date("1899-12-31") %>%
+        ##     sprintf("%s", .)
+        
         ## Extract geocode data for sdr_name existing in maindat
         geocodeDatInclded <- geocodeDat[geocodeDat$name %in% datasetThunk()$sdr_name, ]
 
         ## ggmap
         p <- qmplot(x = gn_longitude, y = gn_latitude, data = geocodeDatInclded,
-                    source = "google")
+                    source = "google") +
+                        labs(title = input$maxdate)
 
         ## Need to print to actually show
         print(p)
