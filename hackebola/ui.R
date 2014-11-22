66a89a06a6459ce47da20d5d45184a80c810dfa6
kaz-yos <kazky@mac.com>
2014-11-22 11:09:40 -0500

    Reformat ui.R

--- hackebola/ui.R
+++ hackebola/ui.R
@@ -12,21 +12,20 @@ ebolaTimeSeries <- read.delim("./data/2.csv", header = TRUE)
 ## ebolaTimeSeries$date <- ebolaTimeSeries$date + as.Date("1899-12-31")
 
 
-
 ### shinyUI
 shinyUI(pageWithSidebar(
 
-    headerPanel("Hack Ebola: Counts Over Time"),
+    headerPanel("Hack Ebola: Occurrence Over Time"),
 
     sidebarPanel(
 
         ## Get the date to plot the cumulative occurrence for
         ## Use this to truncate the data fed to ggmap
         sliderInput('maxdate', 'Date to show cumulative occurrence for: ',
-                    min = min(ebolaTimeSeries$date),
-                    max = max(ebolaTimeSeries$date),
+                    min   = min(ebolaTimeSeries$date),
+                    max   = max(ebolaTimeSeries$date),
                     value = max(ebolaTimeSeries$date),
-                    step = 1, round = 0)
+                    step  = 1, round = 0)
 
     ),
 
