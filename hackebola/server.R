c7662965dc2e3e4bbb4ee7983be3eb7993dbbc1b
kaz-yos <kazky@mac.com>
2014-12-20 10:25:09 -0500

    Change app to use prepared .RData file for efficiency

--- hackebola/server.R
+++ hackebola/server.R
@@ -2,54 +2,65 @@
 library(shiny)
 library(ggplot2)
 library(ggmap)
-library(lubridate)
-library(magrittr)
 library(gridExtra)
-library(dplyr)
 
+
+###
+### Data processing
+if (FALSE) {
+### Load packages for data processing only
+    library(lubridate)
+    library(magrittr)
+    library(dplyr)
 
 ### Load time series dataset
-## Load the data file available at:
-## http://www.qdatum.io/public-sources
-## Use ADM2 except for Liberia (LR) for which only ADM1 are available
-ebolaTimeSeries <- read.delim("./data/2.csv", header = TRUE) %>%
-    subset(., sdr_level == "ADM2" | (country_code == "LR" & sdr_level == "ADM1"))
-
+    ## Load the data file available at:
+    ## http://www.qdatum.io/public-sources
+    ## Use ADM2 except for Liberia (LR) for which only ADM1 are available
+    ebolaTimeSeries <- read.delim("./data/2.csv", header = TRUE) %>%
+        subset(., sdr_level == "ADM2" | (country_code == "LR" & sdr_level == "ADM1"))
 
 ### Load geocode dataset
-geocodeDat <- read.delim("./data/1.csv", header = TRUE) %>%
-    subset(., ((country_code %in% c("GN","LR","SL") & level %in% c("ADM2")) |
-                   (country_code %in% c("LR") & level %in% c("ADM1"))
-               ) &
-               ## drop ones in the sea
-               gn_latitude > 0) %>%
-               ## rename
-               rename(.,
-                      latitude = gn_latitude,
-                      longitude = gn_longitude)
-
+    geocodeDat <- read.delim("./data/1.csv", header = TRUE) %>%
+        subset(., ((country_code %in% c("GN","LR","SL") & level %in% c("ADM2")) |
+                       (country_code %in% c("LR") & level %in% c("ADM1"))
+                   ) &
+                       ## drop ones in the sea
+                       gn_latitude > 0) %>%
+                       ## rename
+                       rename(.,
+                              latitude = gn_latitude,
+                              longitude = gn_longitude)
 
 ### Load Ebola Treatment Centres, Isolation Wards Hospitals and Transit Centres
-etcDat <- read.delim("./data/5.csv", header = TRUE) %>%
-    subset(., country %in% c("Liberia","Guinea","Sierra Leone"))
+    etcDat <- read.delim("./data/5.csv", header = TRUE) %>%
+        subset(., country %in% c("Liberia","Guinea","Sierra Leone"))
 
-CleanEtcDates <- function(v) {
-    v[v %in% "N/A"] <- NA
-    v[v %in% "-"]   <- NA
-    v[v %in% "?"]   <- NA
-    v
+    CleanEtcDates <- function(v) {
+        v[v %in% "N/A"] <- NA
+        v[v %in% "-"]   <- NA
+        v[v %in% "?"]   <- NA
+        v
+    }
+    etcDat$centre_opening_date <- CleanEtcDates(etcDat$centre_opening_date) %>%
+        dmy %>% as.Date
+    etcDat$centre_closing_date <- CleanEtcDates(etcDat$centre_closing_date) %>%
+        dmy %>% as.Date
+
+    ## Give very early date for centers without oepning date
+    etcDat$centre_opening_date[is.na(etcDat$centre_opening_date)] <- as.Date("2014-03-01")
+    ## Give very late date for centers without closing date
+    etcDat$centre_closing_date[is.na(etcDat$centre_closing_date)] <- as.Date("2014-12-31")
+
+### Save dataset
+    save.image(file = "./data/data.RData")
 }
-etcDat$centre_opening_date <- CleanEtcDates(etcDat$centre_opening_date) %>%
-    dmy %>% as.Date
-etcDat$centre_closing_date <- CleanEtcDates(etcDat$centre_closing_date) %>%
-    dmy %>% as.Date
 
-## Give very early date for centers without oepning date
-etcDat$centre_opening_date[is.na(etcDat$centre_opening_date)] <- as.Date("2014-03-01")
-## Give very late date for centers without closing date
-etcDat$centre_closing_date[is.na(etcDat$centre_closing_date)] <- as.Date("2014-12-31")
+###
+### Load all data
+load(file = "./data/data.RData")
 
-
+###
 ### Server configuration
 ## shinyServer then gives Shiny the unnamed function in its first argument.
 shinyServer(function(input, output, session) {
